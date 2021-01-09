bcbce407e222f5c63463794764e825f1a8aa6b6f
srujaan <srujans321@gmail.com>
2021-01-09 10:00:39 -0800

    Added Book constructor

---
+++ bookApp/script.js
@@ -0,0 +1,18 @@
+// Array to store book objects
+
+let myLibrary = [];
+
+// Book constructor
+function Book(author, title, genre, pages) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+    this.pages = pages;
+}
+
+let newBook = new Book('Dan brown', 'Origin', 'Fiction', 450);
+
+// adding books to the library
+function addBookToLibrary() {
+    
+}
