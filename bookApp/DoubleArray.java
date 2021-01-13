dc9c33fe35eacd25fbb21aec682044b962a6feca
srujaan <srujans321@gmail.com>
2021-01-13 09:15:25 -0800

    Double array java

---
+++ bookApp/DoubleArray.java
@@ -0,0 +1,16 @@
+/**
+ * array
+ */
+public class DoubleArray {
+    public static void main(String[] args) {
+        double[][] a = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = 0.0;
+                System.out.println(a[i][j]);
+
+            }
+        } 
+        
+    }
+}
